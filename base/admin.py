03a79671a285395a2ede1dd7ce30f11bf11b6f01
Hyunwoo Kim <khw0726@kaist.ac.kr>
2020-11-20 11:09:44 +0900

    update admin page

--- base/admin.py
+++ base/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Moment
+from .models import Moment, Survey, Log
 # Register your models here.
 admin.site.register(Moment)
+admin.site.register(Survey)
+admin.site.register(Log)

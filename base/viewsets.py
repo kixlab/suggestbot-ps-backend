a0031aecaf7ab2afaf0c7fecf4164577f76a05bc
Hyunwoo Kim <khw0726@kaist.ac.kr>
2021-01-27 13:11:34 +0900

    exact

--- base/viewsets.py
+++ base/viewsets.py
@@ -50,13 +50,14 @@ class MomentViewSet(viewsets.ModelViewSet):
     response['Content-Disposition'] = 'attachment; filename=%s-moments.csv' % dataset_id
     writer = csv.writer(response)
 
-    headers = ['username', 'condition', 'dataset', 'timestamp', 'speaker', 'line', 'direction', 'agree_cnt', 'reason', 'possible_comment', 'created_at']
+    headers = ['username', 'condition', 'dataset', 'timestamp', 'speaker', 'line', 'direction', 'agree_cnt', 'exact_cnt', 'reason', 'possible_comment', 'created_at']
 
     writer.writerow(headers)
 
     for moment in Moment.objects.filter(dataset = dataset):
       agree_cnt = Moment.objects.filter(dataset = dataset, line = moment.line, direction = moment.direction).count()
-      row = [moment.author.username, moment.author.first_name, moment.dataset.dataset_id, moment.line.starttime, moment.line.speaker, moment.line.text, moment.direction, agree_cnt, moment.reason, moment.possible_comment, moment.created_at]
+      exact_cnt = Moment.objects.filter(dataset = dataset, line = moment.line, direction = moment.direction, reason = moment.reason).count()
+      row = [moment.author.username, moment.author.first_name, moment.dataset.dataset_id, moment.line.starttime, moment.line.speaker, moment.line.text, moment.direction, agree_cnt, exact_cnt, moment.reason, moment.possible_comment, moment.created_at]
       writer.writerow(row)
 
     return response
